6ee7ef51dbae7e2d9fa2d54bfea72714da4dcedc
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-11-27 18:36:24 -0300

    [add] create new ticket from webhook

--- src/routes/tickets/index.ts
+++ src/routes/tickets/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { MercadoPagoConfig, Payment, Preference } from "mercadopago";
 import Events from "../../models/events";
+import tickets from "../../models/tickets";
 
 const client = new MercadoPagoConfig({
   accessToken: process.env.MERCADO_PAGO_ACCESS_TOKEN as string,
@@ -11,7 +12,6 @@ const payment = new Payment(client);
 const router = Router();
 
 router.post("/create-ticket", async (req, res) => {
-
   const { email, name, dni, quantity, id } = req.body;
 
   try {
@@ -31,44 +31,49 @@ router.post("/create-ticket", async (req, res) => {
         failure: "https://rumbas-maryjo.onrender.com/",
       },
       payer: {
-        email
+        email,
       },
       metadata: {
         dni,
         email,
         name,
         quantity,
-        eventId: id
+        eventId: id,
       },
-      notification_url: "https://rumbas-maryjo-backend.onrender.com/api/tickets/save-ticket",
+      notification_url:
+        "https://rumbas-maryjo-backend.onrender.com/api/tickets/save-ticket",
     };
     const preference = await new Preference(client);
     const response = await preference.create({ body } as any);
-    res.json(response)
-  } catch (error) {
-
-  }
+    res.json(response);
+  } catch (error) {}
 });
 
 router.post("/save-ticket", async (req, res) => {
   const { topic } = req.query;
-  const id = req.query.id as unknown as string
-  console.log('entro a /save-ticket');
+  const id = req.query.id as unknown as string;
+  console.log("entro a /save-ticket");
 
-  if (topic === "payment") { // Si la notificación es de tipo pago
-      try {
-          const paymentResponse = await payment.get({id}); // Consultas el pago en MP
-          console.log("paymentResponse", paymentResponse); // Aquí tienes todos los detalles del pago
-
-          // Procesa la información del pago según tus necesidades
-      } catch (error) {
-          console.error("Error al consultar el pago:", error);
-      }
+  if (topic === "payment") {
+    // Si la notificación es de tipo pago
+    try {
+      const { metadata } = await payment.get({ id }); // Consultas el pago en MP
+      const newTicket = new tickets({
+        event_id: metadata.event_id,
+        quantity: metadata.quantity,
+        name: metadata.name,
+        dni: metadata.name,
+        email: metadata.email,
+      });
+      await newTicket.save();
+      res.sendStatus(200);
+      // Procesa la información del pago según tus necesidades
+    } catch (error) {
+      console.error("Error al consultar el pago:", error);
+    }
   }
 
   res.sendStatus(200); // Confirmas recepción de la notificación
 });
-
-
 
 export default router;
